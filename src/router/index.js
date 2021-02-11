9ae1554f9fc9c5b1b6e89f52c7c78d3aec018805
Lars Richter <github@n-develop.com>
2021-02-11 17:43:20 +0100

    Moving to vue 3

--- src/router/index.js
+++ src/router/index.js
@@ -1,33 +1,35 @@
-import Vue from 'vue';
-import Router from 'vue-router';
+import {createRouter, createWebHashHistory} from 'vue-router';
 import HomePage from '@/pages/HomePage.vue';
 import CertificatesPage from '@/pages/CertificatesPage.vue';
 import EducationPage from '@/pages/EducationPage.vue';
 import TalksPage from '@/pages/TalksPage.vue';
 
-Vue.use(Router);
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: HomePage,
+  },
+  {
+    path: '/certification',
+    name: 'Cert',
+    component: CertificatesPage,
+  },
+  {
+    path: '/education',
+    name: 'Edu',
+    component: EducationPage,
+  },
+  {
+    path: '/talks',
+    name: 'Talks',
+    component: TalksPage,
+  },
+];
 
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'Home',
-      component: HomePage,
-    },
-    {
-      path: '/certification',
-      name: 'Cert',
-      component: CertificatesPage,
-    },
-    {
-      path: '/education',
-      name: 'Edu',
-      component: EducationPage,
-    },
-    {
-      path: '/talks',
-      name: 'Talks',
-      component: TalksPage,
-    },
-  ],
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
 });
+
+export default router;
